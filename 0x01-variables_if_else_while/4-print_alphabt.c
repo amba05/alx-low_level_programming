ff39a9f813fd18e876fad3dccff7c184c8b16e55
amba05 <80679383+amba05@users.noreply.github.com>
2022-03-11 21:51:00 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,12 +12,8 @@ int main(void)
 	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
-        {
-	if (alpha != 'e' || alpha != 'q')
-        {
+	if (alpha != 'e' && alpha != 'q')
 		putchar(alpha);
-        }
-        }
 
 	putchar('\n');
 	return (0);

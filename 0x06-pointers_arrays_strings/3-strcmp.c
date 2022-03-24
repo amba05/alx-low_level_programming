ce74502b8d631ba5ca8ae6c4ac2780e626b91b21
amba05 <80679383+amba05@users.noreply.github.com>
2022-03-24 21:05:37 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,23 @@
 #include "main.h"
 
 /**
- *_strcmp - compare string src to string dest.
- *@s1: source.
- *@s2: destination.
- *Return: the difference.
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int k = 0, l = 0;
-
-
-	if (s1[k] == s2[k])
+	while (*s1 == *s2)
 	{
-		k++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-
-	else
-	{
-		l = s1[k] - s2[k];
-	}
-
-
-	return (l);
+	return (*s1 - *s2);
 }

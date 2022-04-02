b388e04ce187a1bff80c627d4a90269fce3a63f9
amba05 <80679383+amba05@users.noreply.github.com>
2022-04-02 04:24:42 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-
 /**
- *main: function
- *@argc - argument count
- *@argv: argument value
- *description: prints an argument name
- *Return: 0.
- */
-
-int main (int argc, char *argv[])
+* main - prints name of file
+* @argc: number of arguments
+* @argv: arguments entered
+* Description: print name of file
+* Return: zero
+**/
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	argc = argc;
-
-	return 0;
-
+printf("%s\n", argv[0]);
+argc = argc;
+return (0);
 }

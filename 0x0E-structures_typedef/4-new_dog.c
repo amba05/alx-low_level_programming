3f961cfebabc1d55449479a5ce5252e6596af2ca
Amba <80679383+amba05@users.noreply.github.com>
2022-04-12 02:18:39 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,42 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * init_dog - initializes a variable of type struct dog.
- * @d: struct dog.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: owner of the dog.
- *
- * Return: no return.
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+ * new_dog - creates a new dog.
+ * @name: pointer to a char for name of dog
+ * @age: age of dog
+ * @owner: pointer to a char for owner of dog
+ * Return: pointer to a new dog of type dog_t
+ **/
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d)
+	int nameLen, ownerLen, i;
+	dog_t *d1;
+
+	d1 = (dog_t *)malloc(sizeof(dog_t));
+	if (d1 == NULL)
+		return (NULL);
+	nameLen = ownerLen = 0;
+	while (name[nameLen++])
+		;
+	while (owner[ownerLen++])
+		;
+	d1->name = malloc(nameLen * sizeof(d1->name));
+	if (d1->name == NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		free(d1);
+		return (NULL);
 	}
+	for (i = 0; i <= nameLen; i++)
+		d1->name[i] = name[i];
+	d1->age = age;
+	d1->owner = malloc(ownerLen * sizeof(d1->owner));
+	if (d1->owner == NULL)
+	{
+		free(d1->name);
+		free(d1);
+		return (NULL);
+	}
+	for (i = 0; i <= ownerLen; i++)
+		d1->owner[i] = owner[i];
+	return (d1);
 }

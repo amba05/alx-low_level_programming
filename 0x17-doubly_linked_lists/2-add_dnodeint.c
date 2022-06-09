3db4a9bad0d613481a75cd08684921827c384ec1
Amba <80679383+amba05@users.noreply.github.com>
2022-06-10 00:42:36 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,35 +1,27 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - check the code
- *
- * @head: head link
- * @n: integer to insert
- * Return: Always EXIT_SUCCESS.
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: Pointer to address of head
+ * @n: New value
+ * Return: Address of the new element
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new_node;
 
-	if (head == NULL)
-	{
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
 
-	new = malloc(sizeof(dlistint_t));
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *(head);
 
-	if (!new)
-	{
-		return (NULL);
-	}
+	if (*(head) != NULL)
+		(*head)->prev = new_node;
+	*(head) = new_node;
 
-
-	new->n = n;
-
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
+	return (new_node);
 }
